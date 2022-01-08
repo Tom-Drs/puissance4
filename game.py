960d8122c0faf21791a5fde31e924f8362b1a102
Tom-Drs <tomdamerose@gmail.com>
2022-01-08 14:21:27 +0100

    Add docstring

--- game.py
+++ game.py
@@ -9,9 +9,30 @@ class Puissance4:
         self.grid = [0 for _ in range(width * height)]
 
     def get_index(self, row, column):
+        """
+        Method for get index with row and column number.
+        Args:
+            row (int): row number (start at 0).
+            column (int): column number (start at 0).
+
+        Returns:
+            The index (list grid) of the element in row (row arg)
+            and column (column arg).
+        """
         return self.width * row + column
 
     def put_pawn(self, column, color):
+        """
+        Method for put a pawn in the game's grid.
+        Args:
+            column (int): column number (start at 0).
+            color (int): player's number.
+
+        Returns:
+            -1 : if there is an error in input value.
+            True : if pawn was put correctly in the grid and it's a winning move.
+            False: if pawn was put correctly in the grid.
+        """
 
         # Input value error
         try:
@@ -36,14 +57,38 @@ class Puissance4:
         return self.check_win(row, column, color)
 
     def print_grid(self):
+        """
+        Method for clear stdout and print the game's grid in the stdout.
+        """
         os.system("clear")
         for row in range(self.height):
             print([self.get_pawn(row, column) for column in range(self.width)])
 
     def get_pawn(self, row, column):
+        """
+        Method for get a pawn's value in the grid.
+        Args:
+            row (int): row number (start at 0).
+            column (int): column number (start at 0).
+
+        Returns:
+            int : pawn's value.
+        """
         return self.grid[self.get_index(row, column)]
 
     def get_lines(self, row, column):
+        """
+        Method for get vertical, horizontal, left diagonal and right diagonal
+        line with 3 range from row, column.
+
+        Args:
+            row (int): row number (start at 0).
+            column (int): column number (start at 0).
+
+        Returns:
+            list : all lines in a list (vertical, horizontal, left diagonal
+            and right diagonal).
+        """
         vertical_line = [self.get_pawn(index, column)
                          if 0 <= index < self.height else None
                          for index in range(row - 3, row + 4)]
@@ -63,6 +108,17 @@ class Puissance4:
 
     @staticmethod
     def check_line(line, color):
+        """
+        Staticmethod for verify if there are 4 pawns a row with the same color in the line.
+
+        Args:
+            line (list): pawn's list.
+            color (int): color verified.
+
+        Returns:
+            True: if there is a winner with the arg color in the line.
+            False: if there isn't winner with the arg color in the line.
+        """
         count = 0
         left, right = 3, 3
         while line[left-1] == color and left - 1 >= 0:
@@ -74,6 +130,17 @@ class Puissance4:
         return count >= 3
 
     def check_win(self, row, column, color):
+        """
+        Method for verify is there is a winner when a pawn has been put in the grid.
+        Args:
+            row (int): row number (start at 0).
+            column (int): column number (start at 0).
+            color (int): color verified.
+
+        Returns:
+            True: if there is a winner with the arg color.
+            False: if there isn't winner with the arg color.
+        """
         lines = self.get_lines(row, column)
         for line in lines:
             if self.check_line(line, color):
@@ -81,4 +148,10 @@ class Puissance4:
         return False
 
     def is_in_grid(self, row, column):
+        """Method for verify if a position is in the grid.
+
+        Returns:
+            True: if the position is inside the grid.
+            False: if the position is outside the grid.
+        """
         return 0 <= row < self.height and 0 <= column < self.width
